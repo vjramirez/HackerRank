1f388fd8d72e9cb1d76087a8fb35c1bc19959cfc
vjramirez <vjramirez@gmail.com>
2017-10-28 22:16:44 -0500

    import java.io.*;
    Queues: A Tale of Two Stacks

---
+++ solutions/Queues A Tale of Two Stacks.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        MyQueue<Integer> queue = new MyQueue<Integer>();
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int operation = scan.nextInt();
+            if (operation == 1) { // enqueue
+              queue.enqueue(scan.nextInt());
+            } else if (operation == 2) { // dequeue
+              queue.dequeue();
+            } else if (operation == 3) { // print/peek
+              System.out.println(queue.peek());
+            }
+        }
+        scan.close();
+    }
+}
+
+class MyQueue<T> extends ArrayList<T> {
+
+    public void enqueue(T i){
+        add(i);
+    }
+
+    public void dequeue(){
+        remove(0);
+    }
+
+    public T peek(){
+        return get(0);
+    }
+}
